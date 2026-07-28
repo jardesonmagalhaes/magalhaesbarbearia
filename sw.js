854b8dafb4c1619d5185465bf1e93798675e790f
jardesonmagalhaes <jardesonmagalhaes@alu.ufc.br>
2026-07-28 18:46:16 -0300

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE_NAME = 'agenda-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
-];
+const CACHE_NAME = 'agenda-barbearia-v2';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
